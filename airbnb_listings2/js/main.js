549b5a1abe8c8b046ab8415a588aec77d17efe96
Margaret M Bertoni <bertoni.m@northeastern.edu>
2025-09-20 11:01:48 -0400

    Loading listing names and descriptions with Bootstrap format

---
+++ airbnb_listings2/js/main.js
@@ -0,0 +1,68 @@
+function MainModule(listingsID = "#listings") {
+  const me = {};
+
+
+  const listingsElement = document.querySelector(listingsID);
+
+  function getListingCode(listing) {
+    return `<div class="col-6">
+  <div class="listing card">
+    <img
+      src=${listing.picture_url}
+      class="card-img-top"
+      alt="AirBNB Listing"
+    />
+    <div class="card-body">
+      <h2 class="card-title">${listing.name}</h2>
+      <div>${listing.price}</div>
+      <p class="card-text">
+       ${listing.description}
+      </p>
+     
+    </div>
+    <div>
+      <ul class = "list-group list-group-flush>
+        <li class="list-group-item"></li>
+        
+      
+      </ul>
+    </div>
+  </div>
+  <!-- /card -->
+  </div>
+
+  `;
+  }
+
+  function redraw(listings) {
+    listingsElement.innerHTML = "";
+    // for (let i = 0; i < listings.length; i++) {
+    //   listingsElement.innerHTML += getListingCode(listings[i]);
+    // }
+
+    // for (let listing of listings) {
+    //   console.log("listing", listing );
+    //   listingsElement.innerHTML += getListingCode(listing);
+    // }
+
+    listingsElement.innerHTML = listings.map(getListingCode).join("\n");
+  }
+
+  async function loadData() {
+    const res = await fetch("./airbnb_sf_listings_500.json");
+    const listings = await res.json();
+
+
+    me.redraw(listings.slice(0, 50));
+  }
+
+  me.redraw = redraw;
+  me.loadData = loadData;
+
+  return me;
+}
+
+const main = MainModule();
+
+
+main.loadData();
